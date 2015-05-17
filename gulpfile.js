a9b08e63bca4bfa7b2426b406c2b16d45a9da9ea
Kevin <kevin@pixelkojan.se>
2015-05-17 21:30:19 +0200

    including gulpfile.js and package.json in root

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('scripts', function() {
+  return gulp.src('css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('production/css'));
+});
+
+
+
+gulp.task('default', ['scripts']);
+
+
+
